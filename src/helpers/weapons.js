503d0f88e5a01c03b58a9ba2c66426f9bb08eaef
Victor Mendes <victorutdsim7@gmail.com>
2022-08-12 13:38:04 -0300

    objeto com armas [incompleto]

---
+++ src/helpers/weapons.js
@@ -0,0 +1,114 @@
+const weapons = {
+  pistol: {
+    'P2000': {
+      image: 'P2000.webp',
+    },
+    'USP-S': {
+      image: 'USP-S.webp',
+    },
+    'GLOCK-18': {
+      image: 'Glock-18.webp',
+    },
+    'DUAL-BERETTAS': {
+      image: 'Dual_Berettas.webp',
+    },
+    'P250': {
+      image: 'P250.webp',
+    },
+    'FIVE-SEVEN': {
+      image: 'Five-SeveN.webp',
+    },
+    'TEC-9': {
+      image: 'Tec-9.webp',
+    },
+    'CZ75-AUTO': {
+      image: 'CZ75-Auto.webp',
+    },
+    'DESERT-EAGLE': {
+      image: 'Desert_Eagle.webp',
+    },
+    'R8-REVOLVER': {
+      image: 'R8_Revolver.webp',
+    }
+  },
+  smg: {
+    'MP9': {
+      image: 'MP9.webp',
+    },
+    'MAC-10': {
+      image: 'MAC-10.webp',
+    },
+    'MP7': {
+      image: 'MP7.webp',
+    },
+    'MP5-SD': {
+      image: 'MP5-SD.webp',
+    },
+    'UMP-45': {
+      image: 'UMP-45.webp',
+    },
+    'P90': {
+      image: 'P90.webp',
+    },
+    'PP-BIZON': {
+      image: 'PP-Bizon.webp',
+    },
+  },
+  heavy: {
+    'NOVA': {
+
+    },
+    'XM1014': {
+
+    },
+    'SAWED-OFF': {
+
+    },
+    'MAG-7': {
+
+    },
+    'M249': {
+
+    },
+    'NEGEV': {
+
+    },
+  },
+  rifle: {
+    'AK-47': {
+
+    },
+    'FAMAS': {
+
+    },
+    'GALIL-AR': {
+ 
+    },
+    'M4A4': {
+
+    },
+    'M4A1-S': {
+
+    },
+    'SSG-08': {
+
+    },
+    'AUG': {
+
+    },
+    'SG-553': {
+
+    },
+    'AWP': {
+
+    },
+    'SCAR-20': {
+
+    },
+    'G3SG1': {
+
+    },
+  },
+}
+
+console.log(weapons.pistol.P200);
--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
 const weaponsContainer = document.getElementById('weapons-container');
 const weapons = document.getElementById('weapons');
 const weaponType = document.getElementById('weapon-type')
+
